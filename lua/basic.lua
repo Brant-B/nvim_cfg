e8c628d0eebca6a7652dca967532104c0aaec9a0
李亚鹏 <liyapeng@LYPs-M1.local>
2024-06-07 00:09:50 +0800

    06070009

---
+++ lua/basic.lua
@@ -0,0 +1,5 @@
+-- Make line numbers default
+vim.opt.number = true
+-- You can also add relative line numbers, to help with jumping.
+--  Experiment for yourself to see if you like it!
+vim.opt.relativenumber = true
