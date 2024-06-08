d2377bd3a4e8b7005429f4f657c5220485fce9b7
李亚鹏 <liyapeng@LYPs-M1.local>
2024-06-08 11:15:00 +0800

    on dev branch

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -8,23 +8,14 @@ require("lazy").setup({
 	-- `opts = {}` 用来强制一个插件必须加载
 	{ "numToStr/Comment.nvim", opts = {} }, -- 将选中部分做注释
 	-- NOTE: 3. 插件可以通过`require` 的方式添加
-	require("kickstart/plugins/gitsigns"),
-
-	require("kickstart/plugins/which-key"),
-
-	require("kickstart/plugins/telescope"),
-
-	require("kickstart/plugins/lspconfig"),
-
-	require("kickstart/plugins/conform"),
-
-	require("kickstart/plugins/cmp"),
-
-	require("kickstart/plugins/tokyonight"),
-
-	require("kickstart/plugins/todo-comments"),
-
-	require("kickstart/plugins/mini"),
-
-	require("kickstart/plugins/treesitter"),
+	require("plugins/gitsigns"),  -- 用来添加git 相关信息到编辑器中
+	require("plugins/which-key"), -- 快捷键提示工具
+	require("plugins/telescope"), -- 模糊搜索器
+	require("plugins/lspconfig"), -- 自动补全和悬浮显示功能
+	require("plugins/conform"),   -- TODO 不知道干什么的
+	require("plugins/cmp"),       -- 自动补全工具
+	require("plugins/tokyonight"),-- 东京夜，色彩风格
+	require("plugins/todo-comments"), -- 高亮待处理事项
+	require("plugins/mini"),      -- TODO 不知道干什么的
+	require("plugins/treesitter"),-- 代码解析器
 })
